1bb414f497b1343043a0b5ba583339f7bfffc03a
Sutirtha Chakraborty <sutirtha.chakraborty@xperi.com>
2025-04-09 02:38:27 +0530

    Updated new pages

---
+++ settings.js
@@ -0,0 +1,61 @@
+/**
+ * Must Tiffin Configuration
+ * 
+ * This file contains all configurable settings for the Must Tiffin website.
+ * Update this file to change Google Form links, contact information, and other settings.
+ */
+
+const siteSettings = {
+    // Site Information
+    siteName: "Must Tiffin",
+    tagline: "Authentic Indian Tiffin Service in Dublin",
+    phone: "+353 894376007",
+    email: "info@musttiffin.com",
+    address: "123 Spice Street, Flavortown, Dublin",
+    businessHours: "Mon-Sun: 11:00 AM - 10:00 PM",
+    
+    // Social Media Links
+    socialMedia: {
+        facebook: "https://facebook.com/musttiffin",
+        instagram: "https://instagram.com/musttiffin",
+        twitter: "https://twitter.com/musttiffin"
+    },
+    
+    // Google Form Links
+    forms: {
+        tiffinService: "https://docs.google.com/forms/d/e/1FAIpQLSflCZKVtN2udJjMASuA0jdSHwDK7mVFAvuvpDO-j4FR_9XRQQ/viewform?embedded=true",
+        eventCatering: "https://docs.google.com/forms/d/e/YOUR_EVENT_CATERING_FORM_ID/viewform?embedded=true",
+        corporateCatering: "https://docs.google.com/forms/d/e/YOUR_CORPORATE_CATERING_FORM_ID/viewform?embedded=true",
+        feedback: "https://docs.google.com/forms/d/e/YOUR_FEEDBACK_FORM_ID/viewform?embedded=true",
+        newsletter: "https://docs.google.com/forms/d/e/YOUR_NEWSLETTER_FORM_ID/viewform?embedded=true"
+    },
+    
+    // Cultural Phrases
+    culturalPhrases: {
+        hindi: [
+            "खाना ही जीवन है! (Food is life!)",
+            "पेट पूजा करो! (Worship the stomach!)",
+            "खाओ, पीओ, खुश रहो! (Eat, drink, be happy!)"
+        ],
+        irish: [
+            "May the fork be with you!",
+            "Good food is good mood!",
+            "Life happens, curry helps!"
+        ],
+        southIndian: [
+            "Sapadu ready! (Food is ready!)",
+            "Ruchi ulla saapadu! (Tasty food!)",
+            "Veedu illa, Hotel illa, Must Tiffin dhaan! (Not home, not hotel, it's Must Tiffin!)"
+        ],
+        marathi: [
+            "जेवण झालं का? (Have you eaten yet?)",
+            "चवीष्ट जेवण! (Tasty food!)",
+            "आमचा डब्बा, तुमचं पोट! (Our tiffin, your stomach!)"
+        ],
+        bengali: [
+            "पेट पूजो! (Worship your stomach!)",
+            "खाबार समय होएचे! (It's time to eat!)",
+            "एक बार खाबे, बार बार आशबे! (Eat once, come back again and again!)"
+        ]
+    }
+};
